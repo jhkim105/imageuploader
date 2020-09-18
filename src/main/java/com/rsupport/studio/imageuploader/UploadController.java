d3d400d3c374cb1528dde344c058e0b86ffa5c6c
jhkim105 <just0323>
2020-09-18 18:31:13 +0900

    image download

--- src/main/java/com/rsupport/studio/imageuploader/UploadController.java
+++ src/main/java/com/rsupport/studio/imageuploader/UploadController.java
@@ -2,10 +2,16 @@ package com.rsupport.studio.imageuploader;
 
 import com.rsupport.studio.imageuploader.UploadDto.UploadResponse;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,5 +48,31 @@ public class UploadController {
 
     return ResponseEntity.ok(UploadResponse.of(url));
   }
+
+  @GetMapping(value = "/**/*.jpg", produces = MediaType.IMAGE_JPEG_VALUE)
+  public byte[] downloadJpg(HttpServletRequest request) {
+    return getImage(request);
+
+  }
+
+  @GetMapping(value = "/**/*.png", produces = MediaType.IMAGE_PNG_VALUE)
+  public byte[] downloadPng(HttpServletRequest request) {
+    return getImage(request);
+  }
+
+
+  private byte[] getImage(HttpServletRequest request) {
+    int index = request.getRequestURI().indexOf("upload");
+    String imageUri = request.getRequestURI().substring(index + "upload".length() + 1);
+    String imagePath = String.format("%s/images/%s", System.getProperty(SystemPropertiesConfig.STORAGE_PATH), imageUri);
+    try {
+      return Files.readAllBytes(Paths.get(imagePath));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+
 
 }
